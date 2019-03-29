b13325a32f0117d202f26d60a1c1e5d400e6e00f
Rahul Deewan <rahuldeewanrd@gmail.com>
2019-03-29 23:04:14 +0530

    profile pages added

---
+++ authentication/routes/profile-routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const isLoggedIn = (req, res, next) => {
+  if (!req.user) {
+    res.redirect('/auth/login');
+  } else {
+    next();
+  }
+}
+
+router.get('/', isLoggedIn, (req, res) => {
+  res.render('profile', { 'user': req.user });
+});
+
+module.exports = router;
